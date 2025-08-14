e680aaa9b3049871f4892a67d36163de119728e8
djkier <djjustinekier@gmail.com>
2025-08-14 19:36:09 +0800

    Add deployment for github pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/consolidated-dues-tracker/",
   plugins: [
     react(),
     tailwindcss()
